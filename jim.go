75b6259c64f7052f7a8fbe52f8b564ee4254a8a7
andrewsap <andrewsapw@gmail.com>
2023-12-22 15:05:36 +0300

    refactor: remove go-git dep

--- jim.go
+++ jim.go
@@ -2,27 +2,14 @@ package main
 
 import (
 	"jim/jim"
-	"jim/path"
 	"log"
 	"os"
 	"strings"
 
-	"github.com/go-git/go-billy/v5"
-	"github.com/go-git/go-billy/v5/osfs"
-	"github.com/go-git/go-git/v5/plumbing/cache"
-	"github.com/go-git/go-git/v5/storage/filesystem"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	var currentPath = path.CurrentPath()
-	var wt, dot billy.Filesystem
-
-	wt = osfs.New(currentPath)
-	dot, _ = wt.Chroot(".git")
-
-	storage := filesystem.NewStorage(dot, cache.NewObjectLRUDefault())
-
 	app := &cli.App{
 		Commands: []*cli.Command{
 			{
@@ -30,7 +17,7 @@ func main() {
 				Aliases: []string{"i"},
 				Usage:   "init repository in current directory",
 				Action: func(cCtx *cli.Context) error {
-					jim.Init(storage, dot)
+					jim.Init()
 					return nil
 				},
 			},
