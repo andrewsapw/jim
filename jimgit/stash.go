e184b799be7093311a8b91e7ca90412f4b9aca15
andrewsap <andrewsapw@gmail.com>
2023-12-23 23:01:29 +0400

    refactor: remove stash messages

--- jimgit/stash.go
+++ jimgit/stash.go
@@ -10,11 +10,8 @@ func GetStashIndex(stashName string) (int, error) {
 	getStashesCommand := "stash list"
 	stashesStr := execute.RunGitCommand(getStashesCommand, true)
 
-	fmt.Println(stashesStr)
-
 	// find stash index by name
 	for index, stash := range strings.Split(stashesStr, "\n") {
-		fmt.Println(stash)
 		if strings.Contains(stash, stashName) {
 			return index, nil
 		}
@@ -31,7 +28,6 @@ func CreateStash(stashName string) {
 func DeleteStashByName(stashName string) {
 	stashIndex, err := GetStashIndex(stashName)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
@@ -51,7 +47,6 @@ func PopStashByIndex(stashIndex int) {
 func PopStashByName(stashName string) {
 	stashIndex, err := GetStashIndex(stashName)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
